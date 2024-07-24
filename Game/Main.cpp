f4b0fef3fbde2ffe9d8bc11ece6abf17b23a60a7
k1gs <69785306+k1gs@users.noreply.github.com>
2024-07-24 23:28:52 +0500

    Update Main.cpp

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -3,39 +3,46 @@
 #include <stdlib.h>
 
 using namespace std;
+const char NEW_LINE = '\n';
 
-int settings() {
-	cout << "Placeholder";
-	return 1;
+int game(int dif) {
+	if (dif == 1) {
+		cout << "you st";
+	}
 }
-int start(int x) {
+
+int settings(int x, int y) {
+	cout << "Placeholder";
+	x = 10;
+	x = y; 
+}
+void start(int x) {
 	int difficulty;
-	cout << "select your difficulty:\n1 - easy\n2 - medium\n2 - hard\n";
+	cout << "select your difficulty:\n1 - easy\n2 - medium\n3 - hard\n";
 	cin >> difficulty;
 	x = difficulty;
 	switch (difficulty) {
 	case 1: 
 		cout << "you selected easy difficulty";
-			return difficulty;
+		game(1);
+		break;
 	case 2:
 		cout << "you selected medium difficulty";
-		return difficulty;
+		game(2);
+		break;
 	case 3:
 		cout << "you selected hard difficulty";
-		return difficulty;
+		game(3);
+		break;
 	}
 }
-int game() {
-	return 0x2;
-}
-
 int main() {
 	int cursor;
 	cout << "Hi! Select what u want\n1 - start\n2 - settings\n3 - exit\n";
 	cin >> cursor;
 	
 	if (cursor == 3) {
-		exit;
+		exit(1);
 		return 10;
 	} else if (cursor == 2) {
 		settings;
